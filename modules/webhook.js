0bc8491441958802ecbec49acf5821c18a606f77
ccoenraets <ccoenraets@gmail.com>
2016-04-21 13:02:09 -0400

    adding maintenace mode

--- modules/webhook.js
+++ modules/webhook.js
@@ -85,7 +85,9 @@ let handlePost = (req, res) => {
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
         let sender = event.sender.id;
-        if (event.message && event.message.text) {
+        if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
+            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+        } else if (event.message && event.message.text) {
             processText(event.message.text, sender);
         } else if (event.postback) {
             let payload = event.postback.payload.split(",");
